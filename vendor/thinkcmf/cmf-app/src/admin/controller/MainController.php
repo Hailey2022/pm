bdf36815d3b78498a5d0b0399e050998e7e313f0
Honor <honor@gmail.com>
2023-02-20 10:37:31 +0800

    update index ui

--- vendor/thinkcmf/cmf-app/src/admin/controller/MainController.php
+++ vendor/thinkcmf/cmf-app/src/admin/controller/MainController.php
@@ -2,26 +2,25 @@
 namespace app\admin\controller;
 
 use cmf\controller\AdminBaseController;
+use think\facade\Db;
 
 class MainController extends AdminBaseController
 {
-
-    
     public function index()
     {
         $dashboardWidgets = [];
-        $widgets          = cmf_get_option('admin_dashboard_widgets');
+        $widgets = cmf_get_option('admin_dashboard_widgets');
 
         $defaultDashboardWidgets = [
-            '_SystemCmfHub'           => ['name' => 'CmfHub', 'is_system' => 1],
-            '_SystemCmfDocuments'     => ['name' => 'CmfDocuments', 'is_system' => 1],
+            '_SystemCmfHub' => ['name' => 'CmfHub', 'is_system' => 1],
+            '_SystemCmfDocuments' => ['name' => 'CmfDocuments', 'is_system' => 1],
             '_SystemMainContributors' => ['name' => 'MainContributors', 'is_system' => 1],
-            '_SystemContributors'     => ['name' => 'Contributors', 'is_system' => 1],
-            '_SystemCustom1'          => ['name' => 'Custom1', 'is_system' => 1],
-            '_SystemCustom2'          => ['name' => 'Custom2', 'is_system' => 1],
-            '_SystemCustom3'          => ['name' => 'Custom3', 'is_system' => 1],
-            '_SystemCustom4'          => ['name' => 'Custom4', 'is_system' => 1],
-            '_SystemCustom5'          => ['name' => 'Custom5', 'is_system' => 1],
+            '_SystemContributors' => ['name' => 'Contributors', 'is_system' => 1],
+            '_SystemCustom1' => ['name' => 'Custom1', 'is_system' => 1],
+            '_SystemCustom2' => ['name' => 'Custom2', 'is_system' => 1],
+            '_SystemCustom3' => ['name' => 'Custom3', 'is_system' => 1],
+            '_SystemCustom4' => ['name' => 'Custom4', 'is_system' => 1],
+            '_SystemCustom5' => ['name' => 'Custom5', 'is_system' => 1],
         ];
 
         if (empty($widgets)) {
@@ -63,13 +62,56 @@ class MainController extends AdminBaseController
         $this->assign('dashboard_widget_plugins', $dashboardWidgetPlugins);
         $this->assign('has_smtp_setting', empty($smtpSetting) ? false : true);
 
+        $userid = cmf_get_current_admin_id();
+        $res = Db::name('role_user r, pm_role l, pm_user u')
+            ->where('r.user_id = u.id')
+            ->where('r.role_id = l.id')
+            ->where('u.id', $userid)
+            ->find();
+
+        if ($res !== null) {
+            $username = $res['user_login'];
+            $name = $res['name'];
+            $this->assign('username', $username);
+            $this->assign('type', $name);
+        }
+
+        $dailyReportCount = Db::name('contract c, pm_report r')
+            ->where('c.clientId', $userid)
+            ->where('c.contractId = r.contractId')
+            ->where('r.reportTypeId', '1')
+            ->count();
+
+        $monthlyReportCount = Db::name('contract c, pm_report r')
+            ->where('c.clientId', $userid)
+            ->where('c.contractId = r.contractId')
+            ->where('r.reportTypeId', '<>', '1')
+            ->count();
+        $this->assign('dailyReportCount', $dailyReportCount);
+        $this->assign('monthlyReportCount', $monthlyReportCount);
+        $res = Db::name('contract c, pm_report r')
+            ->where('c.clientId', $userid)
+            ->where('c.contractId = r.contractId')
+            ->order('reportTime', 'desc')
+            ->find('reportTime');
+        if ($res != null) {
+            $this->assign('reportTime', "最后上传时间：" . $res['reportTime']);
+        }
+        $res = Db::name('contract c, pm_pics r')
+            ->where('c.clientId', $userid)
+            ->where('c.contractId = r.contractId')
+            ->order('picTime', 'desc')
+            ->column('picTime');
+        if ($res != null) {
+            $this->assign('picTime', "最后上传时间：" . $res['picTime']);
+        }
         return $this->fetch();
     }
 
     public function dashboardWidget()
     {
         $dashboardWidgets = [];
-        $widgets          = $this->request->param('widgets/a');
+        $widgets = $this->request->param('widgets/a');
         if (!empty($widgets)) {
             foreach ($widgets as $widget) {
                 if ($widget['is_system']) {

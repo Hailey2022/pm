74b6dd6a5e4087335bfbd3042083034d24c7bdf8
cc <cc-github@gmail.com>
2023-03-11 10:34:13 +0800

    ignore it

--- vendor/thinkcmf/cmf-app/src/admin/controller/ManagerController.php
+++ vendor/thinkcmf/cmf-app/src/admin/controller/ManagerController.php
@@ -1350,7 +1350,6 @@ class ManagerController extends AdminBaseController
     }
 
     public function deleteIncome()
-    //TODO: 检查支付
     {
         $projectId = $this->request->param('projectId');
         if (!$this->checkProject($projectId)) {
